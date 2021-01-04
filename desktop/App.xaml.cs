b323b1d9b695770cde54026c8aa84233c0930771
chr1s-b <16579127+chr1s-b@users.noreply.github.com>
2021-01-04 16:06:34 +0000

    Networking stuff

--- desktop/App.xaml.cs
+++ desktop/App.xaml.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +17,75 @@ namespace desktop_app
     /// </summary>
     public partial class App : Application
     {
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			MainWindow wnd = new MainWindow();
+			String server_addr = "madversewally.herokuapp.com";
+            String port = "80";
+			if (e.Args.Length == 1)  // override server_addr for debug
+				server_addr = e.Args[0];
+            if (e.Args.Length == 2)
+            { // override server_addr and port for debug
+                server_addr = e.Args[0];
+                port = e.Args[1];
+            }
+			Console.WriteLine("[INFO] Server address set to {0}", server_addr);
+			wnd.Show();
+
+            var taskWebConnect = Task.Run(() => DoClientWebSocket(server_addr,port));
+
+            taskWebConnect.Wait();
+        }
+
+        private static async Task DoClientWebSocket(String addr, String port)
+        {
+            using (ClientWebSocket ws = new ClientWebSocket())
+            {
+                Uri serverUri = new Uri(String.Format("ws://{0}:{1}/", addr, port));
+                var source = new CancellationTokenSource();
+
+                await ws.ConnectAsync(serverUri, source.Token);
+                while (ws.State == WebSocketState.Open)
+                {
+
+                    Console.WriteLine("Connected to" + String.Format("ws://{0}:{1}/", addr, port));
+                    string msg = "CODE_REQ";
+                    ArraySegment<byte> bytesToSend =
+                                new ArraySegment<byte>(Encoding.UTF8.GetBytes(msg));
+                    await ws.SendAsync(bytesToSend, WebSocketMessageType.Text,
+                                        true, source.Token);
+                    //Receive buffer
+                    var receiveBuffer = new byte[128];
+                    //Multipacket response
+                    var offset = 0;
+                    var dataPerPacket = 32;
+                    while (true)
+                    {
+                        ArraySegment<byte> bytesReceived =
+                                  new ArraySegment<byte>(receiveBuffer, offset, dataPerPacket);
+                        WebSocketReceiveResult result = await ws.ReceiveAsync(bytesReceived,
+                                                                      source.Token);
+                        //Partial data received
+                        //Console.WriteLine("Data:{0}", Encoding.UTF8.GetString(receiveBuffer, offset,result.Count));
+                        offset += result.Count;
+                        if (result.EndOfMessage)
+                            break;
+                    }
+
+                    // Recieved full string
+                    String server_prompt = Encoding.UTF8.GetString(receiveBuffer, 0, offset);
+                    Console.WriteLine("Complete response: {0}", server_prompt);
+
+                    switch (server_prompt)
+                    {
+                        case "CODE_RESP": // server returns game code
+                            Console.WriteLine("Recieved game code"+server_prompt);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
--- web/server.js
+++ web/server.js
@@ -1,8 +1,55 @@
 'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+let WSServer = require('ws').Server;
+let server = require('http').createServer();
+let app = require('./websocket_handler.js');
+
+// Create web socket server on top of a regular http server
+let wss = new WSServer({
+
+    server: server
+});
+
+// Also mount the app here
+server.on('request', app);
+
+// define commands (request/response codes etc)
+var commands = {
+    ["SUPER_SECRET_CODE_LOL"]: poo,
+    ["CODE_REQ"]: process_code_req
+}
+
+function poo(conn) {
+    conn.send(JSON.stringify({ answer: 43 }));
+}
+
+function process_code_req(conn) {
+    conn.send("CODERESPONSE.AAAA");
+}
+
+wss.on('connection', function connection(ws) {
+
+    ws.on('message', function incoming(message) {
+
+        console.log(`received: ${message}`);
+
+        if (message in commands) {
+            commands[message](ws);
+        }
+
+        //ws.send(JSON.stringify({answer: 43}));
+    });
+});
+
+const port = process.env.PORT || 1337
+server.listen(port, function () {
+
+    console.log(`http/ws server listening on ${port}`);
+});
+
+setInterval(() => {
+    console.log("ping",wss.clients.length,"clients");
+    wss.clients.forEach((client) => {
+        client.send(new Date().toTimeString());
+    });
+}, 100);
